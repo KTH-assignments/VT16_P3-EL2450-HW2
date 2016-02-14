ce11bc50399c6ce98a51cfdc59a9fead1332d6b0
alexandros filotheou <alefil@kth.se>
2016-02-14 07:02:10 +0100

    Modifies cpu usage graphs
    	modified:   code/inv_pend_three.mdl
    	new file:   code/plot_cpu_usage.m
    	modified:   report/00.main.aux
    	modified:   report/00.main.pdf
    	modified:   report/figures/4.2.tex
    	modified:   report/figures/5.4.tex
    	modified:   report/sections/05.tex

---
+++ code/plot_cpu_usage.m
@@ -0,0 +1,12 @@
+sim('inv_pend_three')
+s1 = sched.data(:,1);
+s2 = sched.data(:,2);
+s3 = sched.data(:,3);
+s1 = sched.data(:,1) - 1;
+s2 = sched.data(:,2) - 2;
+s3 = sched.data(:,3) - 3;
+s1 = s1 > 0.3;
+s2 = s2 > 0.3;
+s3 = s3 > 0.3;
+u = s1 + s2 + s3;
+plot(u)
