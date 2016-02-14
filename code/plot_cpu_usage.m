ffa2478208554a9c7a76adcc33d86f7c6d6f4bef
alexandros filotheou <alefil@kth.se>
2016-02-14 18:33:16 +0100

    Refines response in question 5
    	deleted:    code/5.6_10.1.tex
    	deleted:    code/5.6_10.2.tex
    	deleted:    code/5.6_10.3.tex
    	modified:   code/init_pend_three.m
    	modified:   code/plot_cpu_usage.m
    	modified:   report/00.main.aux
    	modified:   report/00.main.pdf
    	new file:   report/figures/5.6_10.1.tex
    	new file:   report/figures/5.6_10.2.tex
    	new file:   report/figures/5.6_10.3.tex
    	modified:   report/figures/5.gantt.tex
    	modified:   report/sections/05.tex

--- code/plot_cpu_usage.m
+++ code/plot_cpu_usage.m
@@ -1,28 +1,9 @@
-global executionTime
-
-executionTime = 0.006; %executiontime for the controller
-init_pend_three
-sim('inv_pend_three')
-s16 = sched.data(:,1) - 1;
-s26 = sched.data(:,2) - 2;
-s36 = sched.data(:,3) - 3;
-s16 = s16 > 0.3;
-s26 = s26 > 0.3;
-s36 = s36 > 0.3;
-u6 = s16 + s26 + s36;
-figure
-plot(u6)
-
-executionTime = 0.010; %executiontime for the controller
-init_pend_three
-sim('inv_pend_three')
-s110 = sched.data(:,1) - 1;
-s210 = sched.data(:,2) - 2;
-s310 = sched.data(:,3) - 3;
-s110 = s110 > 0.3;
-s210 = s210 > 0.3;
-s310 = s310 > 0.3;
-u10 = s110 + s210 + s310;
-figure
-plot(u10)
-
+sim('inv_pend_three');
+s1 = sched.data(:,1) - 1;
+s2 = sched.data(:,2) - 2;
+s3 = sched.data(:,3) - 3;
+s1 = s1 > 0.3;
+s2 = s2 > 0.3;
+s3 = s3 > 0.3;
+u = s1 + s2 + s3;
+plot(u)
