2e62df462171db6f0b72e613ebbe991f13e1a472
alexandros filotheou <alefil@kth.se>
2016-02-15 03:02:57 +0100

    Adds figures for response of EDF/ Question 6
    	modified:   code/init_pend_three.m
    	modified:   report/00.main.aux
    	modified:   report/00.main.pdf
    	modified:   report/00.main.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.10.1.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.10.1_diff.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.10.2.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.10.2_diff.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.6.1.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.6.1_diff.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.6.2.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.6.2_diff.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.6.3.tex
    	new file:   report/figures/02.earliest_deadline_first/6/6.6.3_diff.tex
    	modified:   report/sections/01.rate_monotonic_scheduling/05.tex
    	modified:   report/sections/02.earliest_deadline_first/05.tex
    	new file:   report/sections/02.earliest_deadline_first/06.tex

--- code/init_pend_three.m
+++ code/init_pend_three.m
@@ -1,11 +1,11 @@
  function init_pend_three
  
 global executionTime
-%executionTime = 0.010; %executiontime for the controller
-executionTime = 0.006; %executiontime for the controller
+%executionTime = 0.006; %executiontime for the controller
+executionTime = 0.010; %executiontime for the controller
 
-%schedulingPolicy = 'prioDM'; %the scheduling policy used in the simulation
-schedulingPolicy = 'prioEDF'; %the scheduling policy used in the simulation
+schedulingPolicy = 'prioDM'; %the scheduling policy used in the simulation
+%schedulingPolicy = 'prioEDF'; %the scheduling policy used in the simulation
 
 ttInitKernel(schedulingPolicy);
 
